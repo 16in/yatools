b33cabf4fddab6e4b380598818c4eb3e02a3ab85
_16in <kmp.16in@gmail.com>
2019-02-11 03:15:29 +0900

    m2h.hの場所を変更

--- release.bat
+++ release.bat
@@ -4,19 +4,32 @@
 @xcopy document\aafile release\aafile\ /S /Q /Y
 @xcopy document\MLT2HTML release\MLT2HTML\ /S /Q /Y
 
+@rem ------------------------------------------------
+@rem   AAFileAccesser
+@rem ------------------------------------------------
 @mkdir .\release\aafile\lib\x86\ .\release\aafile\lib\x64\
+@mkdir .\release\aafile\include\
 @copy .\bin\Win32\Release\yatools.aafile.lib .\release\aafile\lib\x86\yatools.aafile.lib
 @copy .\bin\x64\Release\yatools.aafile.lib .\release\aafile\lib\x64\yatools.aafile.lib
-@xcopy include release\aafile\include\ /S /Q /Y
+@xcopy .\include\aafile .\release\aafile\include\aafile\ /S /Q /Y
+@copy .\include\aatype.h .\release\aafile\include\aatype.h
 
+
+@rem ------------------------------------------------
+@rem   MLT2HTML
+@rem ------------------------------------------------
 @mkdir .\release\MLT2HTML\libm2h\lib\x86\ .\release\MLT2HTML\libm2h\lib\x64\ release\MLT2HTML\libm2h\include\
+
+@rem libm2h
 @copy .\bin\Win32\Release\libM2H.lib .\release\MLT2HTML\libm2h\lib\x86\libM2H.lib
 @copy .\bin\Win32\Release\libM2H.dll .\release\MLT2HTML\libm2h\lib\x86\libM2H.dll
 @copy .\bin\x64\Release\libM2H.lib .\release\MLT2HTML\libm2h\lib\x64\libM2H.lib
 @copy .\bin\x64\Release\libM2H.dll .\release\MLT2HTML\libm2h\lib\x64\libM2H.dll
-@copy source\libm2h\m2h.h release\MLT2HTML\libm2h\include\m2h.h
+@copy .\include\m2h.h .\release\MLT2HTML\libm2h\include\m2h.h
 
+@rem mlt2html
 @copy .\bin\Win32\Release\MLT2HTML.exe .\release\MLT2HTML\MLT2HTML.exe
 @copy .\bin\x64\Release\MLT2HTML.exe .\release\MLT2HTML\MLT2HTML.x64.exe
+
 
 @call .\cleanup.bat
