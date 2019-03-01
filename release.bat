eaa05c74a1f3bd57ac4ecda79e8c01d38d74bf18
_16in <kmp.16in@gmail.com>
2019-03-02 02:36:14 +0900

    出力設定調整

--- release.bat
+++ release.bat
@@ -9,8 +9,8 @@
 @rem ------------------------------------------------
 @mkdir .\release\aafile\lib\x86\ .\release\aafile\lib\x64\
 @mkdir .\release\aafile\include\
-@copy .\bin\Win32\Release\yatools.aafile.lib .\release\aafile\lib\x86\yatools.aafile.lib
-@copy .\bin\x64\Release\yatools.aafile.lib .\release\aafile\lib\x64\yatools.aafile.lib
+@copy .\bin\Release\x86\yatools.aafile.lib .\release\aafile\lib\x86\yatools.aafile.lib
+@copy .\bin\Release\x64\yatools.aafile.lib .\release\aafile\lib\x64\yatools.aafile.lib
 @xcopy .\include\aafile .\release\aafile\include\aafile\ /S /Q /Y
 @copy .\include\aatype.h .\release\aafile\include\aatype.h
 
@@ -21,15 +21,15 @@
 @mkdir .\release\MLT2HTML\libm2h\lib\x86\ .\release\MLT2HTML\libm2h\lib\x64\ release\MLT2HTML\libm2h\include\
 
 @rem libm2h
-@copy .\bin\Win32\Release\libM2H.lib .\release\MLT2HTML\libm2h\lib\x86\libM2H.lib
-@copy .\bin\Win32\Release\libM2H.dll .\release\MLT2HTML\libm2h\lib\x86\libM2H.dll
-@copy .\bin\x64\Release\libM2H.lib .\release\MLT2HTML\libm2h\lib\x64\libM2H.lib
-@copy .\bin\x64\Release\libM2H.dll .\release\MLT2HTML\libm2h\lib\x64\libM2H.dll
+@copy .\bin\Release\x86\libM2H.lib .\release\MLT2HTML\libm2h\lib\x86\libM2H.lib
+@copy .\bin\Release\x86\libM2H.dll .\release\MLT2HTML\libm2h\lib\x86\libM2H.dll
+@copy .\bin\Release\x64\libM2H.lib .\release\MLT2HTML\libm2h\lib\x64\libM2H.lib
+@copy .\bin\Release\x64\libM2H.dll .\release\MLT2HTML\libm2h\lib\x64\libM2H.dll
 @copy .\include\m2h.h .\release\MLT2HTML\libm2h\include\m2h.h
 
 @rem mlt2html
-@copy .\bin\Win32\Release\MLT2HTML.exe .\release\MLT2HTML\MLT2HTML.exe
-@copy .\bin\x64\Release\MLT2HTML.exe .\release\MLT2HTML\MLT2HTML.x64.exe
+@copy .\bin\Release\x86\MLT2HTML.exe .\release\MLT2HTML\MLT2HTML.exe
+@copy .\bin\Release\x64\MLT2HTML.exe .\release\MLT2HTML\MLT2HTML.x64.exe
 
 
 @call .\cleanup.bat
